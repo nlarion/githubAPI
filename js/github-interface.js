61bd8e55465d0abe37365d0fd90672f0cab7d567
Neil Larion <neil.larion@gmail.com>
2016-03-18 09:54:15 -0700

    Connect up the github-interface.js user input and then call github.js getRepos function to return the inputed user repos in the console

--- js/github-interface.js
+++ js/github-interface.js
@@ -2,7 +2,6 @@ var github = require('./../js/github.js');
 
 $(document).ready(function(){
   $('#submitButton').click(function(){
-    console.log($('#userInput').val());
+    github.getRepos($('#userInput').val());
   });
-  github.getRepos();
 });
--- js/github.js
+++ js/github.js
@@ -1,6 +1,6 @@
 var apiKey = require('./../.env').apiKey;
-exports.getRepos = function(){
-  $.get('https://api.github.com/users/nlarion?access_token=' + apiKey).then(function(response){
+exports.getRepos = function(user){
+  $.get('https://api.github.com/users/'+user+'/repos?access_token=' + apiKey).then(function(response){
     console.log(response);
   }).fail(function(error){
     console.log(error.responseJSON.message);
