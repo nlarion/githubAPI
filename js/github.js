01ccdd2d05c2ad77a32cb71f31ca5c46a43bad66
Neil Larion <neil.larion@gmail.com>
2016-03-18 10:22:59 -0700

    Get user image and name to display with their list of repos

--- js/github.js
+++ js/github.js
@@ -1,10 +1,17 @@
 var apiKey = require('./../.env').apiKey;
 exports.getRepos = function(user){
-  $.get('https://api.github.com/users/'+user+'/repos?access_token=' + apiKey).then(function(response){
+  $.get('https://api.github.com/users/'+user+'?access_token=' + apiKey).then(function(response){
     console.log(response);
-    for (var i = 0; i < response.length; i++) {
-      $('#results').prepend('<div>Repo Name: '+response[i].name+'</div><br>');
-    }
+    $('#userResults').prepend('<div><img src="'+response.avatar_url+'"/></div><br>');
+    $('#userResults').append('<div>User Name: '+response.name+'</div><br>');
+    $.get('https://api.github.com/users/'+user+'/repos?access_token=' + apiKey).then(function(response){
+      console.log(response);
+      for (var i = 0; i < response.length; i++) {
+        $('#repoResults').prepend('<div>Repo Name: '+response[i].name+'</div><br>');
+      }
+    }).fail(function(error){
+      console.log(error.responseJSON.message);
+    });
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
