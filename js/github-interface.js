a2930297631986e72c466e728363e89d49dec6bb
Neil Larion <neil.larion@gmail.com>
2016-03-18 10:14:07 -0700

    Get basic site working with api request and page responce for repo names

--- js/github-interface.js
+++ js/github-interface.js
@@ -3,5 +3,6 @@ var github = require('./../js/github.js');
 $(document).ready(function(){
   $('#submitButton').click(function(){
     github.getRepos($('#userInput').val());
+
   });
 });
--- js/github.js
+++ js/github.js
@@ -2,6 +2,9 @@ var apiKey = require('./../.env').apiKey;
 exports.getRepos = function(user){
   $.get('https://api.github.com/users/'+user+'/repos?access_token=' + apiKey).then(function(response){
     console.log(response);
+    for (var i = 0; i < response.length; i++) {
+      $('#results').prepend('<div>Repo Name: '+response[i].name+'</div><br>');
+    }
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
