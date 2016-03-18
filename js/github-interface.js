cf42dcd6dc126ac43fdf199cb08ebda5ec0667f5
Neil Larion <neil.larion@gmail.com>
2016-03-18 09:51:21 -0700

    Setup github-interface.js to get value from user input field

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,6 +1,8 @@
 var github = require('./../js/github.js');
 
 $(document).ready(function(){
-
+  $('#submitButton').click(function(){
+    console.log($('#userInput').val());
+  });
   github.getRepos();
 });
